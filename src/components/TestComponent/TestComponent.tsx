5c32a4a5ef841d01ec4ada6a7293b395a16ffdc3
thiagonunes <thiagonunes@perbank.com.br>
2023-10-16 17:23:54 -0300

    adicionando hookUseEffect

--- src/components/TestComponent/TestComponent.tsx
+++ src/components/TestComponent/TestComponent.tsx
@@ -1,9 +1,20 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import './TestComponent.css'
 
 function TestComponent(props:{name:string}) {
 
     const [age,setAge] = useState(21)
+
+    useEffect(()=>{
+        console.log('componente criado!')
+    },[])
+
+
+    useEffect(()=>{
+        console.log('idade atualizada para:'+age)
+    },[age])
+
+    console.log('segundo log')
 
     return <div className="TestComponent">Test Component - olá {props.name},{age} !!!
     <button onClick={()=> {
