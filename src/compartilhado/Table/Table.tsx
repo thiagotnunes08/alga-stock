9d0605d8e14ff47b8dfc226d1459dc5c92ef43b5
thiagonunes <thiagonunes@perbank.com.br>
2023-10-23 17:06:48 -0300

    adicioando tabela e node-sass

---
+++ src/compartilhado/Table/Table.tsx
@@ -0,0 +1,37 @@
+import React from 'react'
+import './Table.scss'
+
+const Table = () => {
+    return <table className="AppTable">
+      <thead>
+        <tr>
+          <th>Product</th>
+          <th>Price</th>
+          <th className="right">Stock</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td>Cookie</td>
+          <td>$1.25</td>
+          <td className="right">23</td>
+        </tr>
+        <tr>
+          <td>Milk 1L</td>
+          <td>$0.99</td>
+          <td className="right">10</td>
+        </tr>
+        <tr>
+          <td>Cookie</td>
+          <td>$1.25</td>
+          <td className="right">23</td>
+        </tr>
+        <tr>
+          <td>Milk 1L</td>
+          <td>$0.99</td>
+          <td className="right">10</td>
+        </tr>
+      </tbody>
+    </table>
+  }
+export default Table
--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,15 +1,8 @@
 import React from 'react';
 import './App.css';
 import Header from '../Header';
-import Button from '../../compartilhado/Button';
 import Container from '../../compartilhado/Container';
-import Input from '../../compartilhado/Input';
-
-function TestComponent() {
-
-  return <img width="16" src='https://static-00.iconduck.com/assets.00/search-icon-2044x2048-psdrpqwp.png' alt='icone de pesquisa'></img>
-
-}
+import Table from '../../compartilhado/Table';
 
 function App() {
   return (
@@ -17,14 +10,7 @@ function App() {
       <Header title="AlgaStock" />
 
       <Container>
-        <ul>
-        {
-        ['andressa','joao','lucas','mariana'].map((name,index)=> {
-
-          return <li key={index}>{name}</li>
-          
-        })}
-        </ul>
+       <Table />
       </Container>
       </div>
   );
