a3c68b56153148da31f873d7f620c05e6470d855
thiagonunes <thiagonunes@perbank.com.br>
2023-10-23 17:21:23 -0300

    adicioando dados mockados para tabela

--- src/compartilhado/Table/Table.tsx
+++ src/compartilhado/Table/Table.tsx
@@ -1,37 +1,33 @@
 import React from 'react'
 import './Table.scss'
+import Products from './Table.mockdata'
+
+const headers = [
+  { key: 'name', value: 'Product' },
+  { key: 'price', value: 'Price' },
+  { key: 'actions', value: 'Actions' },
+  { key: 'stock', value: 'Available Stock' },
+]
 
 const Table = () => {
-    return <table className="AppTable">
-      <thead>
-        <tr>
-          <th>Product</th>
-          <th>Price</th>
-          <th className="right">Stock</th>
-        </tr>
-      </thead>
-      <tbody>
-        <tr>
-          <td>Cookie</td>
-          <td>$1.25</td>
-          <td className="right">23</td>
-        </tr>
-        <tr>
-          <td>Milk 1L</td>
-          <td>$0.99</td>
-          <td className="right">10</td>
-        </tr>
-        <tr>
-          <td>Cookie</td>
-          <td>$1.25</td>
-          <td className="right">23</td>
-        </tr>
-        <tr>
-          <td>Milk 1L</td>
-          <td>$0.99</td>
-          <td className="right">10</td>
-        </tr>
-      </tbody>
-    </table>
-  }
+  return <table className="AppTable">
+    <thead>
+      <tr>
+        {
+          headers.map(header => <th key={header.key}>{header.value}</th>)
+        }
+      </tr>
+    </thead>
+    <tbody>
+      {
+        Products.map(product => <tr>
+          <td>{ product.name }</td>
+          <td>${product.price}</td>
+          <td className="right">{product.stock}</td>
+        </tr>)
+      }
+    </tbody>
+  </table>
+}
+
 export default Table
