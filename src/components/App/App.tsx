6e82436dbe28eed0a53d23138e9b727b7d77a579
thiagonunes <thiagonunes@perbank.com.br>
2023-11-24 16:57:24 -0300

    propificando a tabela

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -2,17 +2,27 @@ import React from 'react';
 import './App.css';
 import Header from '../Header';
 import Container from '../../compartilhado/Container';
-import Table from '../../compartilhado/Table';
+import Table, { TableHeader } from '../../compartilhado/Table';
+import Products from '../../compartilhado/Table/Table.mockdata';
+
+const headers: TableHeader[] = [
+  { key: 'id', value: '#' },
+  { key: 'name', value: 'Product' },
+  { key: 'price', value: 'Price', right: true },
+  { key: 'stock', value: 'Available Stock', right: true }
+]
 
 function App() {
   return (
     <div className="App">
       <Header title="AlgaStock" />
-
       <Container>
-       <Table />
+        <Table
+          headers={headers}
+          data={Products}
+        />
       </Container>
-      </div>
+    </div>
   );
 }
 
