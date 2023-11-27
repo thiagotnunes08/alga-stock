9aae81dbd72ba3fb84e2bbd56b6bb9d262678e29
thiagonunes <thiagonunes@perbank.com.br>
2023-11-27 17:36:40 -0300

    add table actions

--- src/compartilhado/Table/Table.tsx
+++ src/compartilhado/Table/Table.tsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import './Table.scss'
 import organizeData from '../../utils/organizeDataForTable'
+import Button from '../Button'
 
 export interface TableHeader {
   key: string
@@ -12,10 +13,10 @@ declare interface TableProps {
   data: any[]
 
   enableActions?: boolean
-  
-  onDelete?: (item : any) => void
-  onDetail?: (item : any) => void
-  onEdit?: (item : any) => void
+
+  onDelete?: (item: any) => void
+  onDetail?: (item: any) => void
+  onEdit?: (item: any) => void
 }
 
 const Table: React.FC<TableProps> = (props) => {
@@ -34,6 +35,14 @@ const Table: React.FC<TableProps> = (props) => {
             </th>
           )
         }
+        {
+
+          props.enableActions
+          && <th className='right'>
+            ações
+          </th>
+
+        }
       </tr>
     </thead>
     <tbody>
@@ -46,13 +55,36 @@ const Table: React.FC<TableProps> = (props) => {
                 .map((item, i) =>
                   item !== '$original'
                     ? <td
-                        key={row.$original.id + i}
-                        className={indexedHeaders[item].right ? 'right' : ''}
-                      >
-                        { row[item] }
-                      </td>
+                      key={row.$original.id + i}
+                      className={indexedHeaders[item].right ? 'right' : ''}
+                    >
+                      {row[item]}
+                    </td>
                     : null
                 )
+            }
+            {
+              props.enableActions
+              && <td className="actions right">
+                {
+                  props.onEdit &&
+                  <Button onClick={() => props.onEdit && props.onEdit(row)}>
+                    editar
+                  </Button>
+                }
+                {
+                  props.onEdit &&
+                  <Button onClick={() => props.onDelete && props.onDelete(row)}>
+                    deletar
+                  </Button>
+                }
+                {
+                  props.onEdit &&
+                  <Button onClick={() => props.onDetail && props.onDetail(row)}>
+                    detalhar
+                  </Button>
+                }
+              </td>
             }
           </tr>
         })
--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -43,6 +43,11 @@ function App() {
         <Table
           headers={headers}
           data={products}
+          enableActions
+          onDelete={console.log}
+          onDetail={console.log}
+          onEdit={console.log}
+          
         />
 
         <ProductForm
