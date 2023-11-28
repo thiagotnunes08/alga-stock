b7c47b37a12e3a9ee7638e0dfedb902287442ed2
thiagonunes <thiagonunes@perbank.com.br>
2023-11-28 17:03:32 -0300

    finalizando crud

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -6,7 +6,7 @@ import Container from '../../compartilhado/Container';
 import Table, { TableHeader } from '../../compartilhado/Table';
 import { Product } from '../../compartilhado/Table/Table.mockdata';
 import ProductForm, { ProductCreator } from '../Produtos/ProdutoForm';
-import { getAll } from '../../service/Products.service';
+import { create, getAll, update,deleteSingleProduct } from '../../service/Products.service';
 
 const headers: TableHeader[] = [
   { key: 'id', value: '#' },
@@ -21,35 +21,37 @@ function App() {
   const [products, setProducts] = useState<Product[]>([])
   const [updantingProduct, setUpdatingProduct] = useState<Product | undefined>()
 
+  async function fectData(){
+    const _products = await getAll()
+    setProducts(_products)
+  }
+
   useEffect(()=> {
-
-    async function fectData(){
-      
-      const _products = await getAll()
-      setProducts(_products)
-    }
-
     fectData()
   },[])
 
-  const handleProductSubmit = (product: ProductCreator) => {
-    setProducts([
-      ...products,
-      {
-        _id: String(products.length + 1),
-        ...product
-      }
-    ])
+  const handleProductSubmit  = async (product: ProductCreator) => {
+    try{
+
+      await create(product)
+      fectData()
+    }
+    catch (err) {
+      if (err instanceof Error) 
+        Swal.fire('Oops!', err.message, 'error')
+    }
   }
 
-  const handleProductUpdate = (newProduct: Product) => {
-    setProducts(products.map(product =>
-      product._id === newProduct._id
-        ? newProduct
-        : product
-    ))
-
-    setUpdatingProduct(undefined)
+  const handleProductUpdate = async (newProduct: Product) => {
+     try{
+      await update(newProduct)
+      setUpdatingProduct(undefined)
+      fectData()
+    }
+    catch (err) {
+      if (err instanceof Error) 
+        Swal.fire('Oops!', err.message, 'error')
+    }
   }
 
   const handleProductEdit = (product: Product) => {
@@ -70,21 +72,21 @@ function App() {
       .then((result) => {
         if (result.value) {
           deleteProduct(product._id)
-          Swal.fire(
-            'Deleted!',
-            'Your file has been deleted.',
-            'success'
-          )
         }
       })
-
-
   }
 
-  const deleteProduct = (id: string) => {
+  const deleteProduct = async (id: string) => {
 
-    setProducts(products.filter(p=> p._id != id))
-
+    try{
+      await deleteSingleProduct(id)
+      fectData()
+      Swal.fire('Uhul!', 'Produto deletado com sucesso', 'success')
+    }
+    catch (err) {
+      if (err instanceof Error) 
+        Swal.fire('Oops!', err.message, 'error')
+    }
   }
 
   const handleProductDetail = (product: Product) => {
--- src/service/Products.service.ts
+++ src/service/Products.service.ts
@@ -1,7 +1,26 @@
 import { Product } from '../compartilhado/Table/Table.mockdata'
+import { ProductCreator } from '../components/Produtos/ProdutoForm'
 import http from '../utils/http'
 
 export const getAll = () =>
 http
 .get<Product[]>('http://localhost:3024/products')
 .then(response=> response.data)
+
+
+export const create = (newProdutoForm: ProductCreator) =>
+http
+.post('http://localhost:3024/products',newProdutoForm)
+
+export const update = ({_id,name,price,stock}: Product) =>
+http
+.patch(`http://localhost:3024/products/${_id}`,{
+
+    ...(name && {name}),
+    ...(price && {price}),
+    ...(stock && {stock}),
+})
+
+export const deleteSingleProduct = (id: string) =>
+http
+.delete(`http://localhost:3024/products/${id}`)
