cd094cbc5029f6a74900e375e00e05fa2db56846
thiagonunes <thiagonunes@perbank.com.br>
2023-12-27 11:22:15 -0300

    add: hook useState ao formulario de login

--- src/components/Autenticacao/LoginForm.tsx
+++ src/components/Autenticacao/LoginForm.tsx
@@ -1,19 +1,45 @@
+
 import Button from "../../compartilhado/Button"
 import Form from "../../compartilhado/Form"
 import Input from "../../compartilhado/Input"
+import React, { useState } from 'react'
 
 const LoginForm = () => {
+    const [form, setForm] = useState({
+        user: '',
+        pass: ''
+    })
 
-    return <Form title="Login - AlgaStock">
-        <Input label="username"
-            placeholder="seu_usuario">
-        </Input>
+    const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const { value, name } = event.target
 
-        <Input label="senha"
-            placeholder="sua_senha"
-            type="password">
-        </Input>
+        setForm({
+            ...form,
+            [name]: value
+        })
+    }
 
+    const handleLogin = () => {
+        console.table(form)
+    }
+
+    return <Form title="Login - AlgaStock" onSubmit={handleLogin}>
+
+        <Input
+            label="user"
+            name="user"
+            value={form.user}
+            onChange={handleInputChange}
+        />
+
+        <Input
+            label="senha"
+            name="pass"
+            type="password"
+            value={form.pass}
+            onChange={handleInputChange}
+
+        />
         <Button>
             LOGIN
         </Button>
